79f8a9021cc25b625250d7b449f0f8f5d5e5f7e7
J_J <112606394+bear300811@users.noreply.github.com>
2025-07-07 11:06:34 +0900

    [level 1] Title: 콜라 문제, Time: 0.03 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.cpp
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.cpp
@@ -5,12 +5,12 @@ using namespace std;
 
 int solution(int a, int b, int n) {
     int answer = 0;
-    
+    int c = 0;
     while(n>=a)
     {
-        int ex = (n/a)*b;
-        answer +=ex;
-        n= ex+(n%a);
+        answer += (n/a)*b;
+        c = n%a;
+        n = (n/a)*b+c;
     }
     return answer;
 }
