5857971bdf91067009ce2d27e9db5becefc1a80a
J_J <112606394+bear300811@users.noreply.github.com>
2025-08-28 10:51:08 +0900

    [level 1] Title: 모의고사, Time: 0.11 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.cpp
+++ 프로그래머스/1/42840. 모의고사/모의고사.cpp
@@ -1,37 +1,44 @@
 #include <string>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 vector<int> solution(vector<int> answers) {
     vector<int> answer;
-    int math1[5] = {1,2,3,4,5};
-    int math2[8] = {2,1,2,3,2,4,2,5};
-    int math3[10] = {3,3,1,1,2,2,4,4,5,5};
-    long long answermath1 = 0;
-    long long answermath2 = 0;
-    long long answermath3 = 0;
-    int indexmath1 = 0;
-    int indexmath2 = 0;
-    int indexmath3 = 0;
-    for(int it : answers)
+    vector<int> count = {0,0,0};
+    vector<int> one = {1,2,3,4,5};
+    vector<int> two = {2,1,2,3,2,4,2,5};
+    vector<int> three = {3,3,1,1,2,2,4,4,5,5};
+    for(int i = 0;i<answers.size();++i)
     {
-        if(indexmath1 >4){indexmath1 = 0;}
-        if(indexmath2 >7){indexmath2 = 0;}
-        if(indexmath3 >9){indexmath3 = 0;}
-        if(it == math1[indexmath1]){answermath1++;}
-        if(it == math2[indexmath2]){answermath2++;}
-        if(it == math3[indexmath3]){answermath3++;}
-        indexmath1++;
-        indexmath2++;
-        indexmath3++;
+        if(one[i%5]==answers[i])
+        {
+            count[0]++;
+        }
+        if(two[i%8]==answers[i])
+        {
+            count[1]++;
+        }
+        if(three[i%10]==answers[i])
+        {
+            count[2]++;
+        }
     }
-    if(answermath1>answermath2 && answermath1 > answermath3){answer.push_back(1);}
-    else if(answermath2>answermath1 && answermath2 > answermath3){answer.push_back(2);}
-    else if(answermath3>answermath2 && answermath3 > answermath1){answer.push_back(3);}
-    else if(answermath1 == answermath2 && answermath1 != answermath3){answer.insert(answer.begin(),{1,2});}
-    else if(answermath3 == answermath2 && answermath3 != answermath1){answer.insert(answer.begin(),{2,3});}
-    else if(answermath1 == answermath3 && answermath1 != answermath2){answer.insert(answer.begin(),{1,3});}
-    else if(answermath1 == answermath2 && answermath1 == answermath3){answer.insert(answer.begin(),{1,2,3});}
+    int maxscore = 0;
+    for(int it : count)
+    {
+       if(it > maxscore)
+       {
+           maxscore = it;
+       }
+    }
+    for(int i = 0;i<count.size();++i)
+    {
+        if(maxscore == count[i])
+        {
+            answer.push_back(i+1);
+        }
+    }
     return answer;
 }
